7a6c14b4a3e41ac0eada0bd0826b7635562b5fd4
Sagar Latake <latake01@gmail.com>
2025-10-14 07:16:59 +0530

    Groq LLM chatbit using langchain and langsmit

---
+++ main.py
@@ -0,0 +1,42 @@
+import getpass
+import os
+from dotenv import load_dotenv
+try:
+    # load environment variables from .env file (requires `python-dotenv`)
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    print('env variable loaded...')
+except ImportError:
+    pass
+
+os.environ["LANGSMITH_TRACING"] = "true"
+if "LANGSMITH_API_KEY" not in os.environ:
+    os.environ["LANGSMITH_API_KEY"] = getpass.getpass(
+        prompt="Enter your LangSmith API key (optional): "
+    )
+if "LANGSMITH_PROJECT" not in os.environ:
+    os.environ["LANGSMITH_PROJECT"] = getpass.getpass(
+        prompt='Enter your LangSmith Project Name (default = "default"): '
+    )
+    if not os.environ.get("LANGSMITH_PROJECT"):
+        os.environ["LANGSMITH_PROJECT"] = "default"
+
+
+
+if not os.environ.get("GROQ_API_KEY"):
+  os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for Groq: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("Llama-3.3-70B-Versatile", model_provider="groq")
+
+from langchain_core.messages import HumanMessage, SystemMessage
+
+messages = [
+    SystemMessage(content="Translate the following from English into Italian"),
+    HumanMessage(content="hi!"),
+]
+
+response = model.invoke(messages)
+print(response.content)
